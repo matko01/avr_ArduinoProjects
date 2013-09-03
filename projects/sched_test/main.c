a073698778222df581787394aebf958f756bebda
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-09-03 15:57:34 +0000

    deep in the woods

--- projects/sched_test/main.c
+++ projects/sched_test/main.c
@@ -7,18 +7,37 @@
 #include <avr/interrupt.h>
 
 // tick every 1 ms
-#define SCHED_TICK_FREQUENCY 1000
+#define SCHED_TICK_FREQUENCY 100
+
+void task1(void) {
+	
+	PORTB = 0x00;
+	unsigned int x = 0;
+	for(x = 0; x < 32768; x++);
+	for(x = 0; x < 32768; x++);
+	for(x = 0; x < 32768; x++);
+	PORTB = 0xff;
+
+}
+
+void task2(void) {
+	
+	PORTD = 0x00;
+	unsigned int x = 0;
+	for(x = 0; x < 32768; x++);
+	for(x = 0; x < 32768; x++);
+	for(x = 0; x < 32768; x++);
+	PORTD = 0xff;
+}
 
 
-int main(void)
+int main(void) 
 {
 	serial_init(E_BAUD_115200);	
 	serial_install_interrupts(SERIAL_RX_INTERRUPT);
 	serial_install_stdio();
 	serial_flush();
 
-	// initialize scheduler
-	sched_init(SCHED_TICK_FREQUENCY);
 
 	return 0;
 }
