aa3e5f42dba25f3d4b950dc0a9e7d848ce8c94a2
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-09-04 11:42:31 +0000

    context switching test successful

---
+++ projects/aos_tests/.gdbinit
@@ -0,0 +1,5 @@
+file sched_test
+target remote :1234
+load
+b 35
+c
--- projects/aos_tests/main.c
+++ projects/aos_tests/main.c
@@ -1,5 +1,6 @@
 #include "serial.h"
 #include "aos.h"
+#include "ctx.h"
 
 #include <stdio.h>
 #include <avr/io.h>
@@ -9,37 +10,35 @@
 // tick every 1 ms
 #define SCHED_TICK_FREQUENCY 100
 
-void task1(void) {
-	
-	PORTB = 0x00;
-	_delay_ms(500);
+void task1(void *a_data UNUSED) {
+	while (1) {
+		PORTB = 0x00;
+		_delay_ms(500);
 
-	PORTB = 0xff;
-	_delay_ms(500);
-
+		PORTB = 0xff;
+		_delay_ms(500);
+	}
 }
 
-void task2(void) {
-	
-	PORTD = 0x00;
-	_delay_ms(500);
+void task2(void *a_data UNUSED) {
+	while (1) {
+		PORTD = 0x00;
+		_delay_ms(500);
 
-	PORTD = 0xff;
-	_delay_ms(500);
+		PORTD = 0xff;
+		_delay_ms(500);
+	}
 }
 
 
 int main(void) 
 {
-	serial_init(E_BAUD_115200);	
-	serial_install_interrupts(SERIAL_RX_INTERRUPT);
-	serial_install_stdio();
-	serial_flush();
-
 	aos_init(SCHED_TICK_FREQUENCY);
+	struct task_cb *t1 = aos_task_create(task1, NULL, 128);
 
-	aos_task_create(task1, 128);
-	aos_task_create(task2, 128);
+	SP = (uint16_t)t1->ctx.sp;
+	AOS_CTX_RESTORE();
+	__asm__ volatile ("ret");
 
 	return 0;
 }
