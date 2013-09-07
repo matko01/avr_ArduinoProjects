95fb71a9a8bc5663ca54297c3ff990d40619b523
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-09-07 11:47:30 +0000

    context switching works

--- projects/aos_tests/main.c
+++ projects/aos_tests/main.c
@@ -7,37 +7,41 @@
 #include "timer_common.h"
 
 // tick every 1 ms
-#define SCHED_TICK_FREQUENCY 35
+#define SCHED_TICK_FREQUENCY 100
 
 void task1(void *a_data UNUSED) {
+	uint16_t x = 0;
 	while (1) {
-		PORTB = 0x00;
-		_delay_ms(500);
-
-		PORTB = 0xff;
-		_delay_ms(500);
+		if (!(x%20000)) PORTB ^= 0xff;
+		x++;		
 	}
 }
+
+static uint8_t y = 0;
 
 void task2(void *a_data UNUSED) {
-	while (1) {
-		PORTD = 0x00;
-		_delay_ms(250);
+	uint8_t *yy = (uint8_t *)a_data;
 
-		PORTD = 0xff;
-		_delay_ms(250);
+	while (1) {
+		if (0 == (*yy))
+			PORTD ^= 0xff;
+
+		*yy = *yy + 1;
 	}
 }
 
 
-int main(void) 
-{
+int main(void) {
+
 	DDRB = 0xff;
 	DDRD = 0xff;
 
 	aos_init(SCHED_TICK_FREQUENCY);
-	/* struct task_cb *t1 UNUSED =  */
-	/* 	aos_task_create(task1, NULL, AOS_TASK_PRIORITY_IDLE, 128); */
+	struct task_cb *t1 UNUSED = 
+		aos_task_create(task1, NULL, AOS_TASK_PRIORITY_IDLE, 32);
+
+	struct task_cb *t2 UNUSED =
+		aos_task_create(task2, (void *)&y, AOS_TASK_PRIORITY_IDLE, 32);
 
 	aos_run();
 	return 0;
