b5dfe1e156d12ddab4ecb2326f7b5d17f875b0ba
Tomasz Wisniewski <tomasz.wisni3wski@gmail.com>
2013-09-10 21:04:00 +0000

    semaphores and synchronization needed in order to improve performace

--- projects/aos_tests/main.c
+++ projects/aos_tests/main.c
@@ -23,10 +23,12 @@ void task2(void *a_data UNUSED) {
 	}
 }
 
+struct task_cb *t3;
+
 void task3(void *a_data UNUSED) {
 	uint32_t a = 0;
 	while (1) {
-		if (!(a%32768)) {
+		if (!(a%65536)) {
 			PORTC ^= 0x01;
 		}
 		a++;
@@ -46,16 +48,15 @@ int main(void) {
 
 	aos_init(SCHED_TICK_FREQUENCY);
 
-	/* aos_common_hook_install(AOS_HOOK_IDLE, hook_task); */
+	aos_common_hook_install(AOS_HOOK_IDLE, hook_task);
 
-	struct task_cb *t1 UNUSED = 
-		aos_task_create(task1, NULL, AOS_TASK_PRIORITY_LOW, 32);
+	/* struct task_cb *t1 UNUSED =  */
+	/* 	aos_task_create(task1, NULL, AOS_TASK_PRIORITY_NORMAL, 32); */
 
-	struct task_cb *t2 UNUSED =
-		aos_task_create(task2, NULL, AOS_TASK_PRIORITY_LOW, 32);
+	/* struct task_cb *t2 UNUSED = */
+	/* 	aos_task_create(task2, NULL, AOS_TASK_PRIORITY_NORMAL, 32); */
 
-	struct task_cb *t3 UNUSED =
-		aos_task_create(task3, NULL, AOS_TASK_PRIORITY_HIGH, 32);
+	t3 = aos_task_create(task3, NULL, AOS_TASK_PRIORITY_HIGH, 32);
 
 	aos_run();
 	return 0;
