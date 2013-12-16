caf3257f3cf9ed5cd9e2af6b0f3853beb8a8228f
TW <tomasz@wisni3wski@gmail.com>
2013-12-16 21:38:40 +0000

    proverbs

---
+++ clk/proverb.c
@@ -0,0 +1,286 @@
+#include "proverb.h"
+
+const char proverb_0[] PROGMEM = "To czlowiek czlowiekowi najbardziej potrzebny jest do szczescia";
+const char proverb_1[] PROGMEM = "Milosc jest jak wiatr, raz silniejsza, a raz slabsza";
+const char proverb_2[] PROGMEM = "Kto w sercu ma tylko nienawisc, nigdy nie zazna milosci i szczescia";
+const char proverb_3[] PROGMEM = "Kropla milosci znaczy wiecej niz ocean rozumu";
+const char proverb_4[] PROGMEM = "Szczerosc to najpiekniejsza forma glupoty";
+const char proverb_5[] PROGMEM = "Prawdziwa cnota krytyk sie nie boi";
+const char proverb_6[] PROGMEM = "Maly krok dla czlowieka, ale wielki skok dla ludzkosci";
+const char proverb_7[] PROGMEM = "Nic tak nie boli jak chwile szczescia wspomniane w niedoli";
+const char proverb_8[] PROGMEM = "Byc wolnym to moc nie klamac";
+const char proverb_9[] PROGMEM = "zycie nabiera wartosci dzieki milosci";
+const char proverb_10[] PROGMEM = "Nie probuj zrozumiec wszystkiego, bo wszystko stanie sie niezrozumiale";
+const char proverb_11[] PROGMEM = "Przyjaciol kochamy dla ich wad, bo lubimy, gdy ktos tez ma wady";
+const char proverb_12[] PROGMEM = "Przyjazn jest subtelna rozkosza szlachetnych dusz";
+const char proverb_13[] PROGMEM = "Przyjazn nalezy do natury ducha, a nie posiadania";
+const char proverb_14[] PROGMEM = "Przyjazn rodzi milosc na pustyni samotnosci";
+const char proverb_15[] PROGMEM = "Dobro nie walczy ze zlem, a leczy je";
+const char proverb_16[] PROGMEM = "Jestesmy dobrzy dla drugich, aby sie sobie wiecej podobac";
+const char proverb_17[] PROGMEM = "Tylko szlachetne czyny sa trwale";
+const char proverb_18[] PROGMEM = "Osiagniecia czesto przeslaniaja zle czyny";
+const char proverb_19[] PROGMEM = "Usmiechnij sie Jutro mozesz nie miec czasu";
+const char proverb_20[] PROGMEM = "Czas przemija, wypowiedziane slowo pozostaje";
+const char proverb_21[] PROGMEM = "Usmiech jest jak prad, ale wiecej ciepla daje";
+const char proverb_22[] PROGMEM = "Kto nie podejmuje trudow walki,ten ponosi koszty straconych szans";
+const char proverb_23[] PROGMEM = "Czas jest takze zywiolem";
+const char proverb_24[] PROGMEM = "Czas omija miejsca, ktore wspominamy";
+const char proverb_25[] PROGMEM = "Czas jest bezwzglednym rzezbiarzem ludzi";
+const char proverb_26[] PROGMEM = "Poznaj swoja wartosc i zacznij sie docieniac";
+const char proverb_27[] PROGMEM = "lud szczescia w zyciu jest wiecej wart niz worek zlota";
+const char proverb_28[] PROGMEM = "Serce miej otwarte dla wszystkich, zaufaj niewielu";
+const char proverb_29[] PROGMEM = "Wolnosc nie jest ulga, lecz trudem wielkosci";
+const char proverb_30[] PROGMEM = "A co po czyjej wielkosci, jak nie ma w glowie madrosci";
+const char proverb_31[] PROGMEM = "Bez matki nie ma chatki";
+const char proverb_32[] PROGMEM = "Beda takie mrozy, ze przymarznie cap do kozy";
+const char proverb_33[] PROGMEM = "Biedny kupuje jedna kapote, ale na cale zycie";
+const char proverb_34[] PROGMEM = "Bierz nogi za pas, poki jeszcze czas";
+const char proverb_35[] PROGMEM = "Bogatemu to i byk sie ocieli";
+const char proverb_36[] PROGMEM = "Co nie sila, to rozumem";
+const char proverb_37[] PROGMEM = "Co powiesz w sekrecie, to wiedza w calym powiecie";
+const char proverb_38[] PROGMEM = "Co sie nie najesz, to sie i nie nalizesz";
+const char proverb_39[] PROGMEM = "Co sie zle zaczyna, to sie dobrze konczy";
+const char proverb_40[] PROGMEM = "Co w czyim sercu na dnie, to wodka wywiedzie snadnie";
+const char proverb_41[] PROGMEM = "Co w kobiecym sercu na dnie, to i diabel nie odgadnie";
+const char proverb_42[] PROGMEM = "Co za duzo - to niezdrowo";
+const char proverb_43[] PROGMEM = "Gdy kota nie ma, myszy harcuja";
+const char proverb_44[] PROGMEM = "Gdy kwiecien chmurny, a maj z wiatrami - dobry rok przed nami";
+const char proverb_45[] PROGMEM = "Gdy luty zimny i suchy, sierpien bedzie goracy";
+const char proverb_46[] PROGMEM = "Gdy na Wawrzynca (10 VIII) slota trzyma do Gromnic (2 II) lekka zima";
+const char proverb_47[] PROGMEM = "Gdy pada w dniu swietej Anny, pada az do Zuzanny";
+const char proverb_48[] PROGMEM = "Gdy przyjdzie swiety Antoni, znac jabluszko na jabloni";
+const char proverb_49[] PROGMEM = "Gdy sierpien wrzos rozwija, jesien krotka, szybko mija";
+const char proverb_50[] PROGMEM = "Gdy sie czlowiek spieszy, to sie diabel cieszy";
+const char proverb_51[] PROGMEM = "Gdy w sierpniu z polnocy dmucha, zwykle nastaje posucha";
+const char proverb_52[] PROGMEM = "Gdy Zielona Matka [15 VIII] deszcz przynosi, to zwykle na Narodzenie Matki [8 IX] rosi";
+const char proverb_53[] PROGMEM = "Gdy na Gromniczna roztaje, rzadkie beda urodzaje";
+const char proverb_54[] PROGMEM = "Gdy po lipcu goracym sierpien sie ochlodzi, zima twarda i z wielkim sniegiem chodzi";
+const char proverb_55[] PROGMEM = "Gdy pszczola w kwietniu nieruchliwa - wiosna mokra bywa";
+const char proverb_56[] PROGMEM = "Gdy Trzej Krolowie cieplem darza, gospodarze o wiosnie gwarza";
+const char proverb_57[] PROGMEM = "Gdy Wielki Piatek ponury, Wielkanoc bez chmury";
+const char proverb_58[] PROGMEM = "Gdy wrzesien bez deszczow bedzie, w zimie wiatru pelno wszedzie   ";
+const char proverb_59[] PROGMEM = "Kazdy jest kowalem swego losu";
+const char proverb_60[] PROGMEM = "Kazdy kij ma dwa konce";
+const char proverb_61[] PROGMEM = "Kazdy kogut na swych smieciach smielszy";
+const char proverb_62[] PROGMEM = "Kazdy ma to, na co sobie zasluzyl";
+const char proverb_63[] PROGMEM = "Kiedy styczen najmrozniejszy, wtedy roczek najplodniejszy";
+const char proverb_64[] PROGMEM = "Kiedy w styczniu lato, w lecie zima za to";
+const char proverb_65[] PROGMEM = "Na bezrybiu i rak ryba";
+const char proverb_66[] PROGMEM = "Na frasunek dobry trunek";
+const char proverb_67[] PROGMEM = "Na jezyku miod, a w sercu lod";
+const char proverb_68[] PROGMEM = "Nie chwal dnia przed zachodem slonca";
+const char proverb_69[] PROGMEM = "Nie czas zalowac roz, gdy plona lasy";
+const char proverb_70[] PROGMEM = "Nie czlowiek zycie wymyslil, ale takim je zastal";
+const char proverb_71[] PROGMEM = "Nie czyn blizniemu, co tobie niemile";
+const char proverb_72[] PROGMEM = "Raz na wozie, raz pod wozem";
+const char proverb_73[] PROGMEM = "Sam sobie sterem, zeglarzem, okretem";
+const char proverb_74[] PROGMEM = "Samotnemu caly swiat pustynia";
+const char proverb_75[] PROGMEM = "Sen mara - Bog wiara";
+const char proverb_76[] PROGMEM = "Szczesliwi czasu nie licza";
+const char proverb_77[] PROGMEM = "Szewc bez butow chodzi";
+const char proverb_78[] PROGMEM = "Szczescie czlowieka psuje, nieszczescie hartuje";
+const char proverb_79[] PROGMEM = "Szczescie czego mi nie dalo, tego mi nie bedzie bralo";
+const char proverb_80[] PROGMEM = "Szczescie dzis matka, a jutro moze byc macocha";
+const char proverb_81[] PROGMEM = "Szczescie glupim sprzyja";
+const char proverb_82[] PROGMEM = "Ten sie nie myli, kto nic nie robi";
+const char proverb_83[] PROGMEM = "W czerwcu sie okaze, co nam rok da w darze";
+const char proverb_84[] PROGMEM = "W krotkie dzionki pazdziernika wszystko z pola, z sadu znika";
+const char proverb_85[] PROGMEM = "W lipcu dzien mglisty, jesienia czas dzdzysty";
+const char proverb_86[] PROGMEM = "W lipcu upaly - wrzesien doskonaly";
+const char proverb_87[] PROGMEM = "W lipcu upaly, w grudniu mroz ostry i staly";
+const char proverb_88[] PROGMEM = "W listopadzie ziemia ciepla, zima bedzie skrzepla";
+const char proverb_89[] PROGMEM = "W lutym aura burzowa - wiosna rychliwa";
+const char proverb_90[] PROGMEM = "Nie wtykaj kija w mrowisko";
+const char proverb_91[] PROGMEM = "W lutym snieg i mroz staly, czynia w lecie upaly";
+const char proverb_92[] PROGMEM = "W marcu sniezek sieje, a czasem slonko grzeje";
+const char proverb_93[] PROGMEM = "Wstyd bardziej niz plaga boli";
+const char proverb_94[] PROGMEM = "Wszedzie dobrze, ale w domu najlepiej";
+const char proverb_95[] PROGMEM = "Wszedzie dobrze, gdzie nas nie ma";
+const char proverb_96[] PROGMEM = "Wszystko dobre, co sie dobrze konczy";
+const char proverb_97[] PROGMEM = "Wszystko ma swoj koniec";
+const char proverb_98[] PROGMEM = "Nie wszystko zloto, co sie swieci";
+const char proverb_99[] PROGMEM = "Zgoda buduje, niezgoda rujnuje";
+const char proverb_100[] PROGMEM = "Ziarnko do ziarnka, az zbierze sie miarka";
+const char proverb_101[] PROGMEM = "Zlego diabli nie biora";
+const char proverb_102[] PROGMEM = "Zlej baletnicy szkodzi rabek przy spodnicy";
+const char proverb_103[] PROGMEM = "Zrob dzis, co masz zrobic jutro                          ";
+const char proverb_104[] PROGMEM = "W Edwarda jesien twarda";
+const char proverb_105[] PROGMEM = "Od swietej Salomei zima jest w nadziei";
+const char proverb_106[] PROGMEM = "Na swieta Elzbiete bywa snieg po piete";
+const char proverb_107[] PROGMEM = "Jak na Jana od Krzyza pogoda sie glosi, taka Nowy Rok przynosi";
+const char proverb_108[] PROGMEM = "Jak sie Katarzyna glosi, tak sie Nowy Rok nosi";
+const char proverb_109[] PROGMEM = "Od swietej Katarzyny nie przesladuj juz zwierzyny";
+const char proverb_110[] PROGMEM = "Gdy swiety Andrzej ze sniegiem przybiezy, sto dni snieg na polu lezy";
+const char proverb_111[] PROGMEM = "Gdy swiety Andrzej poleje, popruszy - caly rok nie w pore role moczy, suszy";
+const char proverb_112[] PROGMEM = "Na swietego Andrzeja dziewkom z wrozby nadzieja";
+const char proverb_113[] PROGMEM = "sniegi Andrzejowe zbozu nie sa zdrowe";
+const char proverb_114[] PROGMEM = "Deszcz w poczatku listopada, mrozy w styczniu zapowiada";
+const char proverb_115[] PROGMEM = "Deszcze listopadowe budza wiatry zimowe";
+const char proverb_116[] PROGMEM = "Slonce listopada mrozy zapowiada";
+const char proverb_117[] PROGMEM = "Gdy listopad z deszczem, grudzien zwykle z wiatrem";
+const char proverb_118[] PROGMEM = "Gdy w listopadzie lisc na szczytach drzew trzyma, to w maju na nowe liscie spadnie jeszcze zima";
+const char proverb_119[] PROGMEM = "Grzmot listopada duzo zboza zapowiada";
+const char proverb_120[] PROGMEM = "Jaka pogoda listopadowa, taka i marcowa";
+const char proverb_121[] PROGMEM = "Grudzien gdy lagodny wszedzie, cala zima dzieckiem bedzie";
+const char proverb_122[] PROGMEM = "Grudzien mokry, w plusku caly, rok nieurodzajny nam zaklada";
+const char proverb_123[] PROGMEM = "Grudzien z sniegiem zimny, zytom na wskros przyjemny";
+const char proverb_124[] PROGMEM = "Barbara po wodzie - Gody po lodzie";
+const char proverb_125[] PROGMEM = "Barbara swieta o gornikach pamieta";
+const char proverb_126[] PROGMEM = "swieta Barbara po lodzie - Boze Narodzenie po wodzie";
+const char proverb_127[] PROGMEM = "Na Mikolaja porzuc woz, a zaprzez sanie";
+const char proverb_128[] PROGMEM = "Na swietego Mikolaja rozbestwia sie wilkow zgraja";
+const char proverb_129[] PROGMEM = "Jaki Marcin taka zima";
+const char proverb_130[] PROGMEM = "Jesli na Marcina sucho, to Gody z plucha";
+const char proverb_131[] PROGMEM = "Na swiety Ambrozy trzymaj dobrze kozy";
+const char proverb_132[] PROGMEM = "swieta Lucy dnia przyrzuci albo go ukroci";
+const char proverb_133[] PROGMEM = "Jaka pogoda Tomaszowa, taka bedzie i majowa";
+const char proverb_134[] PROGMEM = "Na swietego Tomasza najdluzsza noc nasza";
+const char proverb_135[] PROGMEM = "Adam z Ewa pokazuja, jaki styczen i luty po nich nastepuja";
+const char proverb_136[] PROGMEM = "Gdy w Adama i Ewy mroz i pieknie, zima wczesnie peknie";
+const char proverb_137[] PROGMEM = "Na Adama i Ewy czas przyszyc cholewy";
+const char proverb_138[] PROGMEM = "Dobra gospodyni - dom wesolym czyni";
+const char proverb_139[] PROGMEM = "Dobre wychowanie - wrocic rano na sniadanie";
+
+
+const char* const g_proverbs[] PROGMEM = {
+	proverb_0,
+	proverb_1,
+	proverb_2,
+	proverb_3,
+	proverb_4,
+	proverb_5,
+	proverb_6,
+	proverb_7,
+	proverb_8,
+	proverb_9,
+	proverb_10,
+	proverb_11,
+	proverb_12,
+	proverb_13,
+	proverb_14,
+	proverb_15,
+	proverb_16,
+	proverb_17,
+	proverb_18,
+	proverb_19,
+	proverb_20,
+	proverb_21,
+	proverb_22,
+	proverb_23,
+	proverb_24,
+	proverb_25,
+	proverb_26,
+	proverb_27,
+	proverb_28,
+	proverb_29,
+	proverb_30,
+	proverb_31,
+	proverb_32,
+	proverb_33,
+	proverb_34,
+	proverb_35,
+	proverb_36,
+	proverb_37,
+	proverb_38,
+	proverb_39,
+	proverb_40,
+	proverb_41,
+	proverb_42,
+	proverb_43,
+	proverb_44,
+	proverb_45,
+	proverb_46,
+	proverb_47,
+	proverb_48,
+	proverb_49,
+	proverb_50,
+	proverb_51,
+	proverb_52,
+	proverb_53,
+	proverb_54,
+	proverb_55,
+	proverb_56,
+	proverb_57,
+	proverb_58,
+	proverb_59,
+	proverb_60,
+	proverb_61,
+	proverb_62,
+	proverb_63,
+	proverb_64,
+	proverb_65,
+	proverb_66,
+	proverb_67,
+	proverb_68,
+	proverb_69,
+	proverb_70,
+	proverb_71,
+	proverb_72,
+	proverb_73,
+	proverb_74,
+	proverb_75,
+	proverb_76,
+	proverb_77,
+	proverb_78,
+	proverb_79,
+	proverb_80,
+	proverb_81,
+	proverb_82,
+	proverb_83,
+	proverb_84,
+	proverb_85,
+	proverb_86,
+	proverb_87,
+	proverb_88,
+	proverb_89,
+	proverb_90,
+	proverb_91,
+	proverb_92,
+	proverb_93,
+	proverb_94,
+	proverb_95,
+	proverb_96,
+	proverb_97,
+	proverb_98,
+	proverb_99,
+	proverb_100,
+	proverb_101,
+	proverb_102,
+	proverb_103,
+	proverb_104,
+	proverb_105,
+	proverb_106,
+	proverb_107,
+	proverb_108,
+	proverb_109,
+	proverb_110,
+	proverb_111,
+	proverb_112,
+	proverb_113,
+	proverb_114,
+	proverb_115,
+	proverb_116,
+	proverb_117,
+	proverb_118,
+	proverb_119,
+	proverb_120,
+	proverb_121,
+	proverb_122,
+	proverb_123,
+	proverb_124,
+	proverb_125,
+	proverb_126,
+	proverb_127,
+	proverb_128,
+	proverb_129,
+	proverb_130,
+	proverb_131,
+	proverb_132,
+	proverb_133,
+	proverb_134,
+	proverb_135,
+	proverb_136,
+	proverb_137,
+	proverb_138,
+	proverb_139
+};
---
+++ clk/proverb.h
@@ -0,0 +1,8 @@
+#ifndef __PROVERB_H__
+#define __PROVERB_H__
+
+#include <avr/pgmspace.h>
+
+extern const char* const g_proverbs[] PROGMEM;
+
+#endif /* __PROVERB_H__ */
