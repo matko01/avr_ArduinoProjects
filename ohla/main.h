10235ecfa9b842ff1486fc91aade1fa864891c7a
Tomasz Wisniewski <tomasz.wisniewski2@bsykb.com>
2014-01-23 13:30:23 +0000

    open hardware logic analyzer created

---
+++ ohla/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "pca.h"
+
+/**
+ * @brief main prototype (in order to avoid returing a value)
+ */
+void main(void) __attribute__ ((noreturn));
+
+#endif /* __MAIN_H__ */
