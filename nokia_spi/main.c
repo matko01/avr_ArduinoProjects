1a0bb80b4260c0a6023736dfb2ed19397b640e29
TW <tomasz@wisni3wski@gmail.com>
2014-04-26 11:06:37 +0100

    plasma effect

--- nokia_spi/main.c
+++ nokia_spi/main.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <util/delay.h>
+#include <math.h>
 
 #include "main.h"
 #include "pca.h"
@@ -12,7 +13,7 @@
 int main(void)
 {
 	struct dev_pcd8544_ctx lcd;
-	uint8_t buff[(PCD8544_W * PCD8544_H) >> 3] = {0x00};
+	uint8_t buff = 0;
 	uint8_t i = 0;
 
 	lcd.bus = &g_bus_spi_hw_poll;
@@ -26,25 +27,39 @@ int main(void)
 	lcd.res.port = &PORTB;
 	lcd.res.pin = PORTB2;
 
-	spi_hw_poll_init(E_SPI_MODE_MASTER, E_SPI_SPEED_F4);
+	spi_hw_poll_init(E_SPI_MODE_MASTER, E_SPI_SPEED_F2);
 	pcd8544_init(&lcd);
 	pcd8544_clrscr(&lcd);
 	pcd8544_install_stdout(&lcd);
 
+	uint8_t c = 0;
+	uint8_t yt = 0;
+	uint8_t sin_table[256] = {0x00};
+
+	for (uint16_t i = 0; i < 256; i++) {
+		sin_table[i] = (cos(i/2)*sin(i))*31 + 32;
+	}
+
 	while(1) {
-		memset(buff, 0x00, sizeof(buff));
 		pcd8544_gotoxy(&lcd, 0, 0);
 		for (uint8_t x = 0; x<PCD8544_W; x++) {
 			for (uint8_t y = 0; y<PCD8544_H; ) {
-				if (!((x + i) & y)) PCD8544_SET_BIT_HIGH(buff, x, y);
-				y++;
 
-				if (!(y%8))
-				pcd8544_putpixel(&lcd, x, (y >> 3) - 1, buff[x + ((y >> 3) - 1)*84]);
+				yt = y >> 3;
+				
+				c = sin_table [ (uint8_t)(i + ((uint8_t)hypot(42 - x, 24 - y) >> 1)) ];
+
+				if (common_ditherPoint(c,x,y)) {
+					buff |= _BV( y - (yt << 3) );
+				}
+
+				if (!(++y & 0x07)) {
+					pcd8544_putblock(&lcd, x, yt, buff);
+					buff = 0;
+				}
 			}
 		}
-		i++;
-		_delay_ms(20);
+		--i;
 	}
 
 	return 0;
